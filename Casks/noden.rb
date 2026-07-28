7c5e343c99c4d316efbfbb0de871155c8a6e3315
Tugay PALA <tugaypala@gmail.com>
2026-07-28 18:32:18 +0300

    Update noden.rb

--- Casks/noden.rb
+++ Casks/noden.rb
@@ -1,6 +1,6 @@
 cask "noden" do
-  version "1.1.6"
-  sha256 "7b524bd82d69300bf1c73fcd64abe79c6b8a00929661ac3db71d1b9938725f07"
+  version "1.1.8"
+  sha256 "d923edb8152f8723aa4e95df1cda77212892f0e0be2d9e60c04a3b35e5b9e9cd"
 
   url "https://noden.useroamteknoloji.com/downloads/Noden-#{version}.dmg"
   name "Noden"
