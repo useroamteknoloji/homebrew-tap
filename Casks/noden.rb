fd75afc0bbf16066ba3be8e4edcfe27a26cae0ab
Tugay PALA <tugaypala@gmail.com>
2026-07-28 16:42:59 +0300

    Update SHA256 checksum for Noden cask

--- Casks/noden.rb
+++ Casks/noden.rb
@@ -1,6 +1,6 @@
 cask "noden" do
   version "1.1.6"
-  sha256 "f209ee6f4150c06457403e09482cbde693677200230617d1df17b0a26f2587c1"
+  sha256 "7b524bd82d69300bf1c73fcd64abe79c6b8a00929661ac3db71d1b9938725f07"
 
   url "https://noden.useroamteknoloji.com/downloads/Noden-#{version}.dmg"
   name "Noden"
